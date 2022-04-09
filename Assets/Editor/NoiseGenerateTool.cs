4fe9b2a0aa92265755e13a29cd4955481fe922e8
James <465021744@qq.com>
2022-04-09 20:24:25 +0800

    ValueNoise

--- Assets/Editor/NoiseGenerateTool.cs
+++ Assets/Editor/NoiseGenerateTool.cs
@@ -45,7 +45,7 @@ public class NoiseGenerateTool : EditorWindow
 
         int offset = 30;
         int previewSize = 256;
-        
+
         if (currentTexture != null)
         {
             GUILayout.BeginArea(new Rect(0, 65, previewSize, previewSize + offset));
@@ -68,16 +68,26 @@ public class NoiseGenerateTool : EditorWindow
             {
                 currentTexture = GenerateTexture();
             }
+
             if (GUILayout.Button("Random"))
             {
-                RandomOffset = seed.Next(0, 100);
+                RandomOffset = seed.Next(0, (int)TextureSize * (int)TextureSize);
                 currentTexture = GenerateTexture();
             }
         }
+        else if (CurrentNoiseType == NoiseType.ValueNoise)
+        {
+            if (GUILayout.Button("Random"))
+            {
+                RandomOffset = seed.Next(0, (int)TextureSize * (int)TextureSize);
+                currentTexture = GenerateTexture();
+            }
+        }
+
         if (GUILayout.Button("Save"))
         {
-            
         }
+
         GUILayout.EndArea();
     }
 
@@ -109,6 +119,8 @@ public class NoiseGenerateTool : EditorWindow
                 return WhiteNoise();
             case NoiseType.PerlinNoise:
                 return PerlinNoise(x, y, size);
+            case NoiseType.ValueNoise:
+                return ValueNoise(x, y);
         }
 
         return Color.black;
@@ -119,9 +131,63 @@ public class NoiseGenerateTool : EditorWindow
         return seed.Next(0, 2) == 0 ? Color.black : Color.white;
     }
 
-    private Color PerlinNoise(int x, int y, int size)   
+    private Color PerlinNoise(int x, int y, int size)
     {
-        float sample = Mathf.PerlinNoise((float) x / size * Scale + RandomOffset, (float) y / size * Scale + RandomOffset);
+        float sample = Mathf.PerlinNoise((float) x / size * Scale + RandomOffset,
+            (float) y / size * Scale + RandomOffset);
         return new Color(sample, sample, sample);
     }
+
+    #region ValueNoise
+
+    private Vector2 X1Y0 = new Vector2(1, 0);
+    private Vector2 X0Y1 = new Vector2(0, 1);
+
+    private Color ValueNoise(int x, int y)
+    {
+        Vector2 i = floor(new Vector2(x + RandomOffset, y + RandomOffset));
+        Vector2 f = fract(new Vector2(x + RandomOffset, y + RandomOffset));
+
+        float a = random(i);
+        float b = random(i + X1Y0);
+        float c = random(i + X0Y1);
+        float d = random(i + Vector2.one);
+
+        Vector2 u = f * f * (new Vector2(3.0f, 3.0f) - 2.0f * f);
+
+        float sample = mix(mix(a, b, u.x), mix(b, c, u.x), u.y);
+        return new Color(sample, sample, sample);
+    }
+
+    #endregion
+
+
+    #region Math
+
+    private Vector2 floor(Vector2 v2)
+    {
+        return new Vector2(Mathf.Floor(v2.x), Mathf.Floor(v2.y));
+    }
+
+    private float fract(float value)
+    {
+        return value - Mathf.Floor(value);
+    }
+
+    private Vector2 fract(Vector2 v2)
+    {
+        return new Vector2(fract(v2.x), fract(v2.y));
+    }
+
+    private float random(Vector2 v2)
+    {
+        return fract(Mathf.Sin(Vector2.Dot(v2, new Vector2(13.0909f, 783.342f))) * 423234.323f);
+    }
+
+    private float mix(float x, float y, float level)
+    {
+        return x * (1 - level) + y * level;
+    }
+
+    #endregion
 }
